e82d50fbc5de4e3860174f9dbc92fa6d4c4be60b
Alan Rosenthal <alan.rosenthal@gmail.com>
2015-02-10 18:28:13 -0500

    inital commit

---
+++ War/app/src/androidTest/java/com/appspot/c_three_games_test/war/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.appspot.c_three_games_test.war;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
